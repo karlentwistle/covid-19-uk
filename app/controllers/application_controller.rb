32fa3e25e1240ee6cddd9bc8feb61ff192c10042
Karl Entwistle <me@karlentwistle.com>
2020-06-16 22:10:06 +0100

    ignore my ip

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 class ApplicationController < ActionController::Base
-  after_action :track_action
+  after_action :track_action, unless: :ignored_ip_address?
 
   def track_action
     VisitEvent.create(controller_name: controller_name, action_name: action_name)
+  end
+
+  def ignored_ip_address?
+    request.remote_ip == ENV['IGNORED_IP']
   end
 end
